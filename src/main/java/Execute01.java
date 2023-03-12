37ef0fcc6457d0485bf27e44f462f7ed295abacd
nuri Kara <nurikara@yahoo.com>
2023-03-12 19:37:05 +0300

    nurikara

---
+++ src/main/java/Execute01.java
@@ -0,0 +1,33 @@
+import java.sql.*;
+
+public class Execute01 {
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+        Class.forName("org.postgresql.Driver");
+
+        Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/JDBC",
+                "postgres",
+                "Emre4344");
+
+        Statement st = con.createStatement();
+        System.out.println("Connection Success");
+
+        ResultSet veri = st.executeQuery("select * from ogrenciler");
+
+        while(veri.next()){
+
+            System.out.println(veri.getInt(1)
+                    +" / "+veri.getString(2)
+                    +" /"+veri.getString(3)
+                    +"/ "+veri.getString(4)
+                    );
+//
+        }
+        con.close();
+         st.close();
+          veri.close();
+
+
+    }
+}
